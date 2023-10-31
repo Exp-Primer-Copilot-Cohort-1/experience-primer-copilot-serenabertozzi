54a65adc17ab916373fa637fa6802b250f5d8d43
Serena Bertozzi <57240276+serenabertozzi@users.noreply.github.com>
2023-10-31 09:58:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 25,
+        skills: ["C#", "JavaScript", "SQL"]
+    };
+
+    console.log(member.name);
+    console.log(member.age);
+    console.log(member.skills);
+    console.log(member.skills[0]);
+    console.log(member.skills[1]);
+    console.log(member.skills[2]);
+}
